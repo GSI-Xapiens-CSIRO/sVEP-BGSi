83fa93cd38fe5eb21a23fc9536fff50a014d566c
titusprasetyo <titusprasetyo@gmail.com>
2025-03-14 07:57:39 +0700

    debugging payload

--- lambda/pluginSift/docker/main.py
+++ lambda/pluginSift/docker/main.py
@@ -1,8 +1,9 @@
 import os
+import json
 import subprocess
-import zipfile
+# import zipfile
 
-from shared.utils import Orchestrator, s3, download_sift_database
+# from shared.utils import Orchestrator, s3, download_sift_database
 
 
 # Environment variables
@@ -12,122 +13,134 @@ SIFT_DATABASE_REFERENCE = os.environ["SIFT_DATABASE_REFERENCE"]
 os.environ["PATH"] += f':{os.environ["LAMBDA_TASK_ROOT"]}'
 
 # [TODO] : Download siftdb
-download_sift_database(BUCKET_NAME, SIFT_DATABASE_REFERENCE)
+# download_sift_database(BUCKET_NAME, SIFT_DATABASE_REFERENCE)
 
 
-def run_sift_anotator(input, db, output):
-    try:
-        result = subprocess.run(
-            [
-                "java",
-                "-jar",
-                "SIFT4G_Annotator.jar",
-                "-c",
-                "-i",
-                input,
-                "-d",
-                db,
-                "-r",
-                output,
-                "-t",
-            ],  # Run the JAR file
-            capture_output=True,
-            text=True,
-            check=True,
-        )
-        return {"statusCode": 200, "body": result.stdout}
-    except subprocess.CalledProcessError as e:
-        return {"statusCode": 500, "body": f"Error: {e.stderr}"}
+# def run_sift_anotator(input, db, output):
+#     try:
+#         result = subprocess.run(
+#             [
+#                 "java",
+#                 "-jar",
+#                 "SIFT4G_Annotator.jar",
+#                 "-c",
+#                 "-i",
+#                 input,
+#                 "-d",
+#                 db,
+#                 "-r",
+#                 output,
+#                 "-t",
+#             ],  # Run the JAR file
+#             capture_output=True,
+#             text=True,
+#             check=True,
+#         )
+#         return {"statusCode": 200, "body": result.stdout}
+#     except subprocess.CalledProcessError as e:
+#         return {"statusCode": 500, "body": f"Error: {e.stderr}"}
 
 
-def parse_sift_output(output_file):
-    annotations = []
-    with open(output_file, "r") as file:
-        for line in file:
-            if line.startswith("#"):  # Skip headers
-                continue
-            parts = line.strip().split("\t")
-            if len(parts) < 5:
-                continue  # Skip invalid lines
+# def parse_sift_output(output_file):
+#     annotations = []
+#     with open(output_file, "r") as file:
+#         for line in file:
+#             if line.startswith("#"):  # Skip headers
+#                 continue
+#             parts = line.strip().split("\t")
+#             if len(parts) < 5:
+#                 continue  # Skip invalid lines
 
-            # Extract relevant SIFT annotations
-            chrom, pos, ref, alt, sift_score = (
-                parts[0],
-                parts[1],
-                parts[3],
-                parts[4],
-                parts[-1],
-            )
+#             # Extract relevant SIFT annotations
+#             chrom, pos, ref, alt, sift_score = (
+#                 parts[0],
+#                 parts[1],
+#                 parts[3],
+#                 parts[4],
+#                 parts[-1],
+#             )
 
-            annotations.append(
-                {
-                    "chrom": chrom,
-                    "pos": pos,
-                    "ref": ref,
-                    "alt": alt,
-                    "sift_score": sift_score,
-                }
-            )
-    return annotations
-
-
-def merge_sift_annotations(original_data, sift_annotations):
-    updated_data = []
-    for row in original_data:
-        chrom, pos, ref, alt = row[0], row[1], row[3], row[4]
-
-        # Find matching SIFT annotation
-        sift_match = next(
-            (s for s in sift_annotations if s["chrom"] == chrom and s["pos"] == pos),
-            None,
-        )
-
-        if sift_match:
-            row.append(sift_match["sift_score"])  # Add new column
-        else:
-            row.append("NA")  # No match
-
-        updated_data.append(row)
-    return updated_data
+#             annotations.append(
+#                 {
+#                     "chrom": chrom,
+#                     "pos": pos,
+#                     "ref": ref,
+#                     "alt": alt,
+#                     "sift_score": sift_score,
+#                 }
+#             )
+#     return annotations
 
 
-def main(event, _):
-    orchestrator = Orchestrator(event)
-    message = orchestrator.message
-    sns_data = message["snsData"]
+# def merge_sift_annotations(original_data, sift_annotations):
+#     updated_data = []
+#     for row in original_data:
+#         chrom, pos, ref, alt = row[0], row[1], row[3], row[4]
 
-    # Save original SNS data to a temporary VCF file
-    sns_data_filename = f"/tmp/{orchestrator.temp_file_name}_sns_data.vcf"
-    with open(sns_data_filename, "w") as sns_file:
-        sns_file.write(sns_data)
+#         # Find matching SIFT annotation
+#         sift_match = next(
+#             (s for s in sift_annotations if s["chrom"] == chrom and s["pos"] == pos),
+#             None,
+#         )
 
-    # Unzip the SIFT database
-    sift_db_zip_path = f"/tmp/{SIFT_DATABASE_REFERENCE}"
-    sift_db_extract_path = f"/tmp/{SIFT_DATABASE_REFERENCE}/DB"
-    with zipfile.ZipFile(sift_db_zip_path, 'r') as zip_ref:
-        zip_ref.extractall(sift_db_extract_path)
+#         if sift_match:
+#             row.append(sift_match["sift_score"])  # Add new column
+#         else:
+#             row.append("NA")  # No match
 
-    # Run the SIFT annotator
-    result = run_sift_anotator(
-        sns_data_filename,
-        f"/tmp/{SIFT_DATABASE_REFERENCE}/DB",
-        orchestrator.temp_file_name,
-    )
+#         updated_data.append(row)
+#     return updated_data
 
-    # Parse SIFT output from `stdout`
-    sift_annotations = parse_sift_output(result["body"])
 
-    # Merge original data with SIFT annotations
-    original_rows = [row.split("\t") for row in sns_data.split("\n") if row]
-    enriched_rows = merge_sift_annotations(original_rows, sift_annotations)
+def main(payload):
+    print(f"Received message: {payload}")
+    event = json.loads(payload)
+    sns = event['Records'][0]['Sns']
+    message = json.loads(sns['Message'])
+    data = message['snsData']
+    request_id = message['requestId']
 
-    # Write enriched data to a TSV file
-    base_filename = orchestrator.temp_file_name
-    tsv_filename = f"/tmp/{base_filename}.tsv"
+    print(f"Data: {data}")
+    print(f"Request ID: {request_id}")
+    print("Processing SIFT data...")
+    return {"statusCode": 200, "body": "Success"}
 
-    with open(tsv_filename, "w") as tsv_file:
-        for row in enriched_rows:
-            tsv_file.write("\t".join(row) + "\n")
+    # orchestrator = Orchestrator(event)
+    # message = orchestrator.message
+    # sns_data = message["snsData"]
 
-    s3.Bucket(SVEP_REGIONS).upload_file(tsv_filename, f"{base_filename}.tsv")
-    orchestrator.mark_completed()
+    # # Save original SNS data to a temporary VCF file
+    # sns_data_filename = f"/tmp/{orchestrator.temp_file_name}_sns_data.vcf"
+    # with open(sns_data_filename, "w") as sns_file:
+    #     sns_file.write(sns_data)
+
+    # # Unzip the SIFT database
+    # sift_db_zip_path = f"/tmp/{SIFT_DATABASE_REFERENCE}"
+    # sift_db_extract_path = f"/tmp/{SIFT_DATABASE_REFERENCE}/DB"
+    # with zipfile.ZipFile(sift_db_zip_path, 'r') as zip_ref:
+    #     zip_ref.extractall(sift_db_extract_path)
+
+    # # Run the SIFT annotator
+    # result = run_sift_anotator(
+    #     sns_data_filename,
+    #     f"/tmp/{SIFT_DATABASE_REFERENCE}/DB",
+    #     orchestrator.temp_file_name,
+    # )
+
+    # # Parse SIFT output from `stdout`
+    # sift_annotations = parse_sift_output(result["body"])
+
+    # # Merge original data with SIFT annotations
+    # original_rows = [row.split("\t") for row in sns_data.split("\n") if row]
+    # enriched_rows = merge_sift_annotations(original_rows, sift_annotations)
+
+    # # Write enriched data to a TSV file
+    # base_filename = orchestrator.temp_file_name
+    # tsv_filename = f"/tmp/{base_filename}.tsv"
+
+    # with open(tsv_filename, "w") as tsv_file:
+    #     for row in enriched_rows:
+    #         tsv_file.write("\t".join(row) + "\n")
+
+    # s3.Bucket(SVEP_REGIONS).upload_file(tsv_filename, f"{base_filename}.tsv")
+    # orchestrator.mark_completed()
