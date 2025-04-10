5027db35e6f1916a23f54fb87a82d1a7a568639a
Erik Firmansyah <Erik.Firmansyah@xapiens.id>
2025-04-10 15:08:37 +0700

    DEV: adding shared python module as lambda layer

--- lambda/qcFigures/docker_prep.py
+++ lambda/qcFigures/docker_prep.py
@@ -26,5 +26,15 @@ def hash_dir(dir_path: str):
 
     return sha.hexdigest()
 
+def update_shared():
+    if os.path.isdir(f"{DIRECTORY}/shared"):
+        shutil.rmtree(f"{DIRECTORY}/shared")
+
+    shutil.copytree(
+        f"{DIRECTORY}/../../shared_resources/python-modules/python/shared",
+        f"{DIRECTORY}/shared",
+    )
+
 if __name__ == "__main__":
+    update_shared()
     print(f""" {{ "hash": "{hash_dir(DIRECTORY)}" }} """)
