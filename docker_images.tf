1ed0320868569c7bdf5d8214b871cdf8ce99dd0b
Fajar Septiawan <Fajar.Septiawan@xapiens.id>
2025-04-11 16:05:10 +0700

    chore: comment build arg

--- docker_images.tf
+++ docker_images.tf
@@ -12,79 +12,79 @@ provider "docker" {
 # pluginConsequence docker image
 #
 data "external" "pluginConsequence_lambda_source_hash" {
-    program = ["python", "lambda/pluginConsequence/docker_prep.py"]
-    working_dir = path.module
+  program     = ["python", "lambda/pluginConsequence/docker_prep.py"]
+  working_dir = path.module
 }
 
 module "docker_image_pluginConsequence_lambda" {
-    source = "terraform-aws-modules/lambda/aws//modules/docker-build"
+  source = "terraform-aws-modules/lambda/aws//modules/docker-build"
 
-    create_ecr_repo = true
-    ecr_repo        = "svep-pluginconsequence-lambda-containers"
-    ecr_repo_lifecycle_policy = jsonencode({
-        "rules" : [
-        {
-            "rulePriority" : 1,
-            "description" : "Keep only the last 1 images",
-            "selection" : {
-            "tagStatus" : "any",
-            "countType" : "imageCountMoreThan",
-            "countNumber" : 1
-            },
-            "action" : {
-            "type" : "expire"
-            }
+  create_ecr_repo = true
+  ecr_repo        = "svep-pluginconsequence-lambda-containers"
+  ecr_repo_lifecycle_policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Keep only the last 1 images",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 1
+        },
+        "action" : {
+          "type" : "expire"
         }
-        ]
-    })
-    use_image_tag = false
-    source_path   = "${path.module}/lambda/pluginConsequence"
+      }
+    ]
+  })
+  use_image_tag = false
+  source_path   = "${path.module}/lambda/pluginConsequence"
 
-    triggers = {
-        dir_sha = data.external.pluginConsequence_lambda_source_hash.result.hash
-    }
+  triggers = {
+    dir_sha = data.external.pluginConsequence_lambda_source_hash.result.hash
+  }
 
-    platform = "linux/amd64"
+  platform = "linux/amd64"
 }
 
 #
 # qcFigures docker image
 #
 data "external" "qcFigures_lambda_source_hash" {
-    program = ["python", "lambda/qcFigures/docker_prep.py"]
-    working_dir = path.module
+  program     = ["python", "lambda/qcFigures/docker_prep.py"]
+  working_dir = path.module
 }
 
 module "docker_image_qcFigures_lambda" {
-    source = "terraform-aws-modules/lambda/aws//modules/docker-build"
+  source = "terraform-aws-modules/lambda/aws//modules/docker-build"
 
-    create_ecr_repo = true
-    ecr_repo        = "svep-qcfigures-lambda-containers"
-    ecr_repo_lifecycle_policy = jsonencode({
-        "rules" : [
-        {
-            "rulePriority" : 1,
-            "description" : "Keep only the last 1 images",
-            "selection" : {
-            "tagStatus" : "any",
-            "countType" : "imageCountMoreThan",
-            "countNumber" : 1
-            },
-            "action" : {
-            "type" : "expire"
-            }
+  create_ecr_repo = true
+  ecr_repo        = "svep-qcfigures-lambda-containers"
+  ecr_repo_lifecycle_policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Keep only the last 1 images",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 1
+        },
+        "action" : {
+          "type" : "expire"
         }
-        ]
-    })
-    use_image_tag = false
-    build_args = {
-        SHARED_LAYER_PATH = "${path.module}/shared_resources/python-modules/python/shared"
-    }
-    source_path   = "${path.module}/lambda/qcFigures"
+      }
+    ]
+  })
+  use_image_tag = false
+  #   build_args = {
+  #     SHARED_LAYER_PATH = "${path.module}/shared_resources/python-modules/python/shared"
+  #   }
+  source_path = "${path.module}/lambda/qcFigures"
 
-    triggers = {
-        dir_sha = data.external.qcFigures_lambda_source_hash.result.hash
-    }
+  triggers = {
+    dir_sha = data.external.qcFigures_lambda_source_hash.result.hash
+  }
 
-    platform = "linux/amd64"
+  platform = "linux/amd64"
 }
