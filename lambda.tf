fcf808eed951bb4c5a720fe86023b9db199c23cf
Erik Firmansyah <Erik.Firmansyah@xapiens.id>
2025-04-15 15:35:29 +0700

    DEV: Fix get lambda function name

--- lambda.tf
+++ lambda.tf
@@ -215,7 +215,7 @@ resource "aws_lambda_permission" "formatOutput_invoke_permission" {
 resource "aws_lambda_permission" "vcfstats_graphic_invoke_permission" {
   statement_id  = "APIVcfstatsAllowInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = module.lambda-qcFigures.function_name
+  function_name = module.lambda-qcFigures.lambda_function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.VPApi.execution_arn}/*/*/${aws_api_gateway_resource.vcfstats.path_part}"
 }
