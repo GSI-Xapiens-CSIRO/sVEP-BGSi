e067cb7a1039f94e307500e7c305e5ef171d4f2d
Erik Firmansyah <Erik.Firmansyah@xapiens.id>
2025-04-15 10:53:00 +0700

    Revert "DEV: comment qc figures lambda"
    This reverts commit a382606010418c2759c1fd710282d2520e453642.

--- docker_images.tf
+++ docker_images.tf
@@ -50,41 +50,41 @@ module "docker_image_pluginConsequence_lambda" {
 #
 # qcFigures docker image
 #
-# data "external" "qcFigures_lambda_source_hash" {
-#     program = ["python", "lambda/qcFigures/docker_prep.py"]
-#     working_dir = path.module
-# }
+data "external" "qcFigures_lambda_source_hash" {
+    program = ["python", "lambda/qcFigures/docker_prep.py"]
+    working_dir = path.module
+}
 
-# module "docker_image_qcFigures_lambda" {
-#     source = "terraform-aws-modules/lambda/aws//modules/docker-build"
+module "docker_image_qcFigures_lambda" {
+    source = "terraform-aws-modules/lambda/aws//modules/docker-build"
 
-#     create_ecr_repo = true
-#     ecr_repo        = "svep-qcfigures-lambda-containers"
-#     ecr_repo_lifecycle_policy = jsonencode({
-#         "rules" : [
-#         {
-#             "rulePriority" : 1,
-#             "description" : "Keep only the last 1 images",
-#             "selection" : {
-#             "tagStatus" : "any",
-#             "countType" : "imageCountMoreThan",
-#             "countNumber" : 1
-#             },
-#             "action" : {
-#             "type" : "expire"
-#             }
-#         }
-#         ]
-#     })
-#     use_image_tag = false
-#     build_args = {
-#         SHARED_LAYER_PATH = "${path.module}/shared_resources/python-modules/python/shared"
-#     }
-#     source_path   = "${path.module}/lambda/qcFigures"
+    create_ecr_repo = true
+    ecr_repo        = "svep-qcfigures-lambda-containers"
+    ecr_repo_lifecycle_policy = jsonencode({
+        "rules" : [
+        {
+            "rulePriority" : 1,
+            "description" : "Keep only the last 1 images",
+            "selection" : {
+            "tagStatus" : "any",
+            "countType" : "imageCountMoreThan",
+            "countNumber" : 1
+            },
+            "action" : {
+            "type" : "expire"
+            }
+        }
+        ]
+    })
+    use_image_tag = false
+    build_args = {
+        SHARED_LAYER_PATH = "${path.module}/shared_resources/python-modules/python/shared"
+    }
+    source_path   = "${path.module}/lambda/qcFigures"
 
-#     triggers = {
-#         dir_sha = data.external.qcFigures_lambda_source_hash.result.hash
-#     }
+    triggers = {
+        dir_sha = data.external.qcFigures_lambda_source_hash.result.hash
+    }
 
-#     platform = "linux/amd64"
-# }
+    platform = "linux/amd64"
+}
