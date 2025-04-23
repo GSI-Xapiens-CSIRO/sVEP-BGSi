681ddfdd880078dc740f897eb06dc1d536cea109
NickEdwards7502 <nickedwards7502@gmail.com>
2025-04-23 01:07:29 +0000

    MAINT: Inherit provider information from top level module
    * Allows conditional enabling

--- docker_images.tf
+++ docker_images.tf
@@ -1,13 +1,3 @@
-data "aws_ecr_authorization_token" "token" {}
-
-provider "docker" {
-  registry_auth {
-    address  = format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, var.region)
-    username = data.aws_ecr_authorization_token.token.user_name
-    password = data.aws_ecr_authorization_token.token.password
-  }
-}
-
 #
 # pluginConsequence docker image
 #
--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-  region = var.region
-  default_tags {
-    tags = var.common-tags
-  }
-}
-
 data "aws_caller_identity" "this" {}
 
 locals {
