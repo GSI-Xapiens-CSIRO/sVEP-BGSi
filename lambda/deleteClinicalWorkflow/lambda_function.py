0c9d701d2e722141eaa9c157215d5a6df2a6753f
Fajar Septiawan <Fajar.Septiawan@xapiens.id>
2025-04-30 11:06:42 +0700

    feat: add send job email

--- lambda/deleteClinicalWorkflow/lambda_function.py
+++ lambda/deleteClinicalWorkflow/lambda_function.py
@@ -1,5 +1,6 @@
 import json
-from shared.dynamodb import scan_pending_jobs, bulk_delete_jobs
+from shared.dynamodb import scan_pending_jobs, bulk_delete_jobs, send_job_email
+import time
 
 
 def lambda_handler(event, _):
@@ -13,3 +14,25 @@ def lambda_handler(event, _):
             print("[Crob Jobs] - Deleting pending jobs.")
             # Delete the pending jobs
             bulk_delete_jobs(pending_jobs)
+
+            for job in pending_jobs:
+                job_id = job["job_id"]["S"]
+                user_id = job.get("uid", {}).get("S")
+                project_name = job.get("project_name", {}).get("S")
+                input_vcf = job.get("input_vcf", {}).get("S")
+                job_status = job.get("job_status", {}).get("S")
+
+                print(
+                    f"[Crob Jobs] - Sending email for job {job_id} to user {user_id}."
+                )
+
+                send_job_email(
+                    job_id=job_id,
+                    job_status=job_status,
+                    project_name=project_name,
+                    input_vcf=input_vcf,
+                    user_id=user_id,
+                    is_from_failed_execution=False,
+                )
+
+                time.sleep(0.5)  # 500 milliseconds
