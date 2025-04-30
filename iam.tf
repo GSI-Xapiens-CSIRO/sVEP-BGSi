609c0f459b875815330615a2a15823c1c112c8fd
Fajar Septiawan <Fajar.Septiawan@xapiens.id>
2025-04-30 12:32:42 +0700

    fix: add s3:ListBucket

--- iam.tf
+++ iam.tf
@@ -1175,6 +1175,7 @@ data "aws_iam_policy_document" "lambda-deleteClinicalWorkflow" {
     actions = [
       "s3:DeleteObject",
       "s3:GetObject",
+      "s3:ListBucket"
     ]
     resources = [
       "${aws_s3_bucket.svep-temp.arn}/*",
