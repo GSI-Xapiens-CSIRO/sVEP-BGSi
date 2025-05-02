a25a492193c59a270b98c299521ed5cdafae296a
Erik Firmansyah <Erik.Firmansyah@xapiens.id>
2025-05-02 10:45:26 +0700

    DEV: Add plugin gnomad 1kg

--- lambda/pluginGnomadOneKG/lambda_function.py
+++ lambda/pluginGnomadOneKG/lambda_function.py
@@ -3,52 +3,47 @@ import os
 
 from shared.utils import (
     CheckedProcess,
-    orchestration,
+    Orchestrator,
+    handle_failed_execution,
+    start_function,
     Timer,
 )
 
 FORMAT_OUTPUT_SNS_TOPIC_ARN = os.environ["FORMAT_OUTPUT_SNS_TOPIC_ARN"]
-PLUGIN_GNOMAD_SNS_TOPIC_ARN = os.environ["PLUGIN_GNOMAD_SNS_TOPIC_ARN"]
-PLUGIN_GNOMAD_ONE_KG_SNS_TOPIC_ARN = os.environ["PLUGIN_GNOMAD_ONE_KG_SNS_TOPIC_ARN"]
-GNOMAD_S3_PREFIX = "https://gnomad-public-us-east-1.s3.amazonaws.com/release/4.1/vcf/genomes/gnomad.genomes.v4.1.sites."
-GNOMAD_S3_SUFFIX = ".vcf.bgz"
-MAX_REGIONS_PER_QUERY = 20  # Hard limit is probably around 5000
-# Just the columns after the identifying columns
-GNOMAD_COLUMNS = {
-    "afAfr": "INFO/AF_afr",
-    "afEas": "INFO/AF_eas",
-    "afFin": "INFO/AF_fin",
-    "afNfe": "INFO/AF_nfe",
-    "afSas": "INFO/AF_sas",
-    "afAmr": "INFO/AF_amr",
-    "af": "INFO/AF",
-    "ac": "INFO/AC",
-    "an": "INFO/AN",
-    "siftMax": "INFO/sift_max",
-}
-FILTER_MAX_MAF = float(os.environ["FILTER_MAX_MAF"])
+PLUGIN_GNOMAD_ONE_KG_SNS_TOPIC_ARN = os.environ["PLUGIN_GNOMAD_ONE_KG_SNS_TOPIC_ARN"]  # reuse or rename
+# Change to appropriate 1KG location
+KGENOMES_S3_PREFIX = "https://1000genomes.s3.amazonaws.com/release/20130502/"
+KGENOMES_S3_SUFFIX = ".vcf.gz"
+
+MAX_REGIONS_PER_QUERY = 20
 MILLISECONDS_BEFORE_SPLIT = 300000
 
+# Adjust these to actual INFO fields available in 1KG files
+KGENOMES_COLUMNS = {
+    "af": "INFO/AF",
+    "ac": "INFO/AC",
+    "an": "INFO/AN"
+}
 
 def get_query_process(regions, ref_chrom):
-    chrom = f"chr{ref_chrom}"
+    chrom = f"chr{ref_chrom}"  # or just use the number if 1KG doesn't use 'chr'
+    vcf_url = f"{KGENOMES_S3_PREFIX}ALL.{ref_chrom}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes{KGENOMES_S3_SUFFIX}"
     args = [
         "bcftools",
         "query",
         "--regions",
         ",".join(f"{chrom}:{region}" for region in regions),
         "--format",
-        f"%POS\t%REF\t%ALT\t{'\\t'.join("%" + val for val in GNOMAD_COLUMNS.values())}\n",
-        f"{GNOMAD_S3_PREFIX}{chrom}{GNOMAD_S3_SUFFIX}",
+        f"%POS\t%REF\t%ALT\t{'\\t'.join('%' + val for val in KGENOMES_COLUMNS.values())}\n",
+        vcf_url,
     ]
-    return CheckedProcess(args, error_message="bcftools error querying gnomAD")
+    return CheckedProcess(args, error_message="bcftools error querying 1000 Genomes")
 
 
 def convert_to_regions_queries(sns_data):
     regions_data = defaultdict(lambda: defaultdict(list))
     for data in sns_data:
         regions_data[data["posVcf"]][(data["refVcf"], data["altVcf"])].append(data)
-    # Split into chunks of MAX_REGIONS_PER_QUERY
     regions_list = sorted(list(regions_data.keys()))
     region_chunks = [
         regions_list[i : i + MAX_REGIONS_PER_QUERY]
@@ -65,7 +60,7 @@ def convert_to_regions_queries(sns_data):
     return region_chunks, chunked_data
 
 
-def add_gnomad_columns(sns_data, ref_chrom, timer):
+def add_1kg_columns(sns_data, ref_chrom, timer):
     region_queries, chunked_data = convert_to_regions_queries(sns_data)
     query_processes = [
         get_query_process(query_region, ref_chrom) for query_region in region_queries
@@ -77,7 +72,6 @@ def add_gnomad_columns(sns_data, ref_chrom, timer):
         query_processes, enumerate(chunked_data)
     ):
         if timer.out_of_time():
-            # Call self with remaining data
             remaining_data = [
                 data
                 for later_region_data in chunked_data[chunk_i:]
@@ -96,9 +90,9 @@ def add_gnomad_columns(sns_data, ref_chrom, timer):
             for data in regions_data[(pos, ref, alt)]:
                 data.update(
                     {
-                        col_name: gnomad_datum
-                        for col_name, gnomad_datum in zip(
-                            list(GNOMAD_COLUMNS.keys()), query_data
+                        col_name: kgenomes_datum
+                        for col_name, kgenomes_datum in zip(
+                            list(KGENOMES_COLUMNS.keys()), query_data
                         )
                     }
                 )
@@ -107,39 +101,43 @@ def add_gnomad_columns(sns_data, ref_chrom, timer):
             [data for variant_data in regions_data.values() for data in variant_data]
         )
         query_process.check()
-    print(f"Updated {lines_updated}/{len(completed_lines)} rows with gnomad data")
-    # Filter out rows with MAF > FILTER_MAX_MAF
-    rare_records = [
-        line_dict
-        for line_dict in completed_lines
-        if float(line_dict.get("af", 0)) <= FILTER_MAX_MAF
-    ]
-    print(
-        f"Passed {len(rare_records)}/{len(completed_lines)} records with af <= {FILTER_MAX_MAF}"
-    )
-    return rare_records, remaining_data
+    print(f"Updated {lines_updated}/{len(completed_lines)} rows with 1000 Genomes data")
+    return completed_lines, remaining_data
 
 
 def lambda_handler(event, context):
     timer = Timer(context, MILLISECONDS_BEFORE_SPLIT)
-    with orchestration(event) as orc:
-        sns_data = orc.message["snsData"]
-        complete_lines, remaining = add_gnomad_columns(sns_data, orc.ref_chrom, timer)
+    orchestrator = Orchestrator(event)
+    message = orchestrator.message
+    sns_data = message["snsData"]
+    ref_chrom = message["refChrom"]
+    request_id = message["requestId"]
+
+    try:
+        complete_lines, remaining = add_1kg_columns(sns_data, ref_chrom, timer)
         if remaining:
             print(f"remaining data length {len(remaining)}")
-            orc.resend_self(
-                message_update={
+            start_function(
+                topic_arn=PLUGIN_GNOMAD_ONE_KG_SNS_TOPIC_ARN,
+                base_filename=orchestrator.temp_file_name,
+                message={
                     "snsData": remaining,
+                    "refChrom": ref_chrom,
+                    "requestId": request_id,
                 },
+                resend=True,
             )
-            assert len(remaining) < len(
-                sns_data
-            ), "Not able to make any progress getting gnomAD data"
-        base_filename = orchestrator.temp_file_name
+            assert (
+                len(complete_lines) > 0
+            ), "Not able to make any progress getting 1KG data"
         start_function(
-            topic_arn=PLUGIN_GNOMAD_ONE_KG_SNS_TOPIC_ARN,
-            base_filename=base_filename,
+            topic_arn=FORMAT_OUTPUT_SNS_TOPIC_ARN,
+            base_filename=orchestrator.temp_file_name,
             message={
                 "snsData": complete_lines,
+                "requestId": request_id,
             },
         )
+        orchestrator.mark_completed()
+    except Exception as e:
+        handle_failed_execution(request_id, e)
