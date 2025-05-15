e73eb19b2319808928c54cf310681e37e04e5e38
Erik Firmansyah <Erik.Firmansyah@xapiens.id>
2025-05-15 11:41:13 +0700

    DEV: Add plugin Gnomad Constraint

---
+++ lambda/pluginGnomadConstraint/lambda_function.py
@@ -0,0 +1,147 @@
+import json
+import csv
+import os
+from collections import defaultdict
+
+from shared.utils import (
+    CheckedProcess,
+    orchestration,
+    Timer,
+    download_to_tmp,
+)
+
+REFERENCE_LOCATION = os.environ["REFERENCE_LOCATION"]
+CONSTRAINT_REFERENCE = os.environ["CONSTRAINT_REFERENCE"]
+GENE_INDEX_REFERENCE = os.environ["GENE_INDEX_REFERENCE"]
+
+MAX_ROW_PER_CHUNK = 300
+MILLISECONDS_BEFORE_SPLIT = 300000
+CONSTRAINT_COLUMNS = {
+    "misZ": 32,
+    "misOe": 27,
+    "misOeCiLower": 29,
+    "misOeCiUpper": 30,
+    "lofPli": 15,
+    "lofOe": 13,
+    "lofOeCiUpper": 19,
+    "lofOeCiLower": 18,
+}
+
+download_to_tmp(REFERENCE_LOCATION, CONSTRAINT_REFERENCE, raise_on_notfound=True)
+download_to_tmp(REFERENCE_LOCATION, GENE_INDEX_REFERENCE, raise_on_notfound=True)
+
+
+def get_query_process(gene_index, index_file, constraint_file, constraint_cache):
+    gene, _ = gene_index.split(":")
+
+    if gene in constraint_cache:
+        return constraint_cache[gene]
+
+    results = []
+
+    if gene in index_file:
+        constraint_file.seek(index_file[gene])
+        results = constraint_file.readlines()
+
+    constraints_data = {}
+    for result in results:
+        line = result.strip()
+        if not line:
+            continue
+        geneName, genId, transcript, *query_data = line.split("\t")
+        if gene == geneName:
+            constraints_data[transcript] = {
+                key: query_data[index] for key, index in CONSTRAINT_COLUMNS.items()
+            }
+        else:
+            break
+    return constraint_cache.setdefault(gene, constraints_data)
+
+
+def convert_to_genes_queries(sns_data):
+    genes_data = defaultdict(list)
+    for data in sns_data:
+        genes_data[data["geneName"]].append(data)
+    genes_list = []
+    chunk_datas = []
+    for key, value in genes_data.items():
+        chunk_data = []
+        i = 0
+        for gene_data in value:
+            if len(chunk_data) == MAX_ROW_PER_CHUNK:
+                gene_index = f"{key}:{i}"
+                genes_list.append(gene_index)
+                chunk_datas.append(chunk_data)
+                chunk_data = []
+                i += 1
+
+            chunk_data.append(gene_data)
+
+        if chunk_data:
+            gene_index = f"{key}:{i}"
+            genes_list.append(gene_index)
+            chunk_datas.append(chunk_data)
+
+    return genes_list, chunk_datas
+
+
+def add_constraint_columns(sns_data, timer):
+    genes_list, chunked_datas = convert_to_genes_queries(sns_data)
+
+    with open(f"/tmp/{GENE_INDEX_REFERENCE}") as injson:
+        index_file = json.load(injson)
+
+    constraint_cache = {}
+    with open(f"/tmp/{CONSTRAINT_REFERENCE}") as constraint_file:
+        query_processes = [
+            get_query_process(gene_index, index_file, constraint_file, constraint_cache)
+            for gene_index in genes_list
+        ]
+
+    lines_updated = 0
+    completed_lines = []
+    remaining_data = []
+
+    for query_process, (chunk_i, gene_datas) in zip(
+        query_processes, enumerate(chunked_datas)
+    ):
+        if timer.out_of_time():
+            remaining_data = [
+                data for datas in chunked_datas[chunk_i:] for data in datas
+            ]
+            break
+
+        for data in gene_datas:
+            transcript = data["transcriptId"]
+            if transcript in query_process:
+                data.update(query_process[transcript])
+                lines_updated += 1
+
+        completed_lines.extend(gene_datas)
+
+    print(
+        f"Updated {lines_updated}/{len(completed_lines)} rows with Constraint Genomes data"
+    )
+    return completed_lines, remaining_data
+
+
+def lambda_handler(event, context):
+    timer = Timer(context, MILLISECONDS_BEFORE_SPLIT)
+    with orchestration(event) as orc:
+        sns_data = orc.message["snsData"]
+        complete_lines, remaining = add_constraint_columns(sns_data, timer)
+        if remaining:
+            print(f"remaining data length {len(remaining)}")
+            orc.resend_self(
+                message_update={
+                    "snsData": remaining,
+                },
+            )
+            assert len(remaining) < len(
+                sns_data
+            ), "Not able to make any progress getting gnomAD data"
+        orc.next_function(
+            message={
+                "snsData": complete_lines,
+            },
+        )
