4827a2c4067fb2a14c43b08512c2e260b08ba004
Brendan Hosking <brendan.hosking@gmail.com>
2025-05-15 03:50:40 +0000

    FIX: Don't upload incomplete clinvar reference
    If there is a problem with the curl command to download the clinvar
    reference file, the reference bedfile is computed over the truncated
    input. This truncated reference is then uploaded, overwriting what was
    probably a more complete reference on S3.
    
    Allow multiple retries of the reference download, and compare the hash
    to ensure it completed successfully. Crash the whole process on any
    errors, and automatically shutdown the EC2 instance on any exit.

--- lambda/updateReferenceFiles/clinvar.sh
+++ lambda/updateReferenceFiles/clinvar.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-set -xuo pipefail
+set -exuo pipefail
+trap 'shutdown -h now' EXIT
 
 FTP_PATH="__FTP_PATH__"
 CLINVAR_FILE="__CLINVAR_FILE__"
 OUTPUT_BED="__OUTPUT_BED__"
 REFERENCE_BUCKET="__REFERENCE_BUCKET__"
+
+FULL_PATH="${FTP_PATH}/${CLINVAR_FILE}"
 
 yum update -y
 yum install -y \
@@ -19,14 +22,17 @@ HTSLIB_VERSION="1.21"
 curl -L https://github.com/samtools/htslib/releases/download/$HTSLIB_VERSION/htslib-$HTSLIB_VERSION.tar.bz2 | tar -xjf -
 cd htslib-$HTSLIB_VERSION
 ./configure  --disable-bz2 --disable-lzma
-make
 make install
 
 cat > xmltobed.py << 'EOF'
 __clinvar_xmltobed.py__
 EOF
 
-curl "${FTP_PATH}/${CLINVAR_FILE}" | bgzip -cd | python3 xmltobed.py | sort -k 1,1 -k 2,2n -k 3,3n | bgzip -c > "${OUTPUT_BED}"
+curl -O "${FULL_PATH}" --retry 9
+curl -O "${FULL_PATH}.md5" --retry 9
+# Remove preceeding path from md5 file
+sed -i 's|\([a-f0-9]\+ \).*/|\1*|' "${CLINVAR_FILE}.md5"
+md5sum -c "${CLINVAR_FILE}.md5"
+bgzip -cd "${CLINVAR_FILE}" | python3 xmltobed.py | sort -k 1,1 -k 2,2n -k 3,3n | bgzip -c > "${OUTPUT_BED}"
 tabix --csi "${OUTPUT_BED}"
 aws s3 cp --recursive --exclude "*" --include "${OUTPUT_BED}*" . "s3://${REFERENCE_BUCKET}/"
-shutdown -h now
